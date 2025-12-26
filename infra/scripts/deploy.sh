fc0adb146d144e3801a20f8c79af5ac0af90538f
Ramesh Jha <39025768+rj695855@users.noreply.github.com>
2025-12-26 14:52:38 +0530

    Update deploy.sh

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+set -e
 
+yum update -y
+yum install -y nginx docker
+
+systemctl enable nginx
+systemctl start nginx
+
+systemctl enable docker
+systemctl start docker
+
+echo "<h1>Production EC2 deployed by Terraform + GitHub Actions</h1>" \
+> /usr/share/nginx/html/index.html
